444afad615d90146442de3850e13e9375e6e1352
lisujun <lisujun@news.cn>
2016-07-28 17:04:41 +0800

    js

--- src/js/config.js
+++ src/js/config.js
@@ -1,19 +1,42 @@
-var config = {
-	read:{
-		articleList:''
-	},
-	getHrefParam:function(_name){
-		var _value = '';
-		if(window.location.href.split('?')[1]){
-			var _paramArr = window.location.href.split('?')[1].split('&');
-			$(_paramArr).each(function(_index,_element){
-				var _param = _element.split('=');
-				if(_param[0]==_name){
-					_value = _param[1];
-				}
-			});
+var tag = false;
+if(tag){
+	var config = {
+		read:{
+			articleList:'/gome-manager-web/reading/queryArticleList'
+		},
+		getHrefParam:function(_name){
+			var _value = '';
+			if(window.location.href.split('?')[1]){
+				var _paramArr = window.location.href.split('?')[1].split('&');
+				$(_paramArr).each(function(_index,_element){
+					var _param = _element.split('=');
+					if(_param[0]==_name){
+						_value = _param[1];
+					}
+				});
+			}
+			
+			return _value;
 		}
-		
-		return _value;
+	}
+}else{
+	var config = {
+		read:{
+			articleList:''
+		},
+		getHrefParam:function(_name){
+			var _value = '';
+			if(window.location.href.split('?')[1]){
+				var _paramArr = window.location.href.split('?')[1].split('&');
+				$(_paramArr).each(function(_index,_element){
+					var _param = _element.split('=');
+					if(_param[0]==_name){
+						_value = _param[1];
+					}
+				});
+			}
+			
+			return _value;
+		}
 	}
 }
