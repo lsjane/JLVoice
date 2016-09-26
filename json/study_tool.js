259b77e66a2a9a61419f04cc92a1654ebc64189d
lisujun <lisujun@news.cn>
2016-09-26 18:33:09 +0800

    临床工具

---
+++ json/study_tool.js
@@ -0,0 +1,185 @@
+{
+    "code": 1,
+    "attach": [
+        {
+            "id": 1,
+            "title": "心内科",
+            "subTitle": [
+                {
+                    "id": 1,
+                    "imgUrl": "1-1",
+                    "title": "血压水平的分类和定义",
+                    "olink": null
+                },
+                {
+                    "id": 2,
+                    "imgUrl": "1-2",
+                    "title": "血脂水平分层标准",
+                    "olink": null
+                },
+                {
+                    "id": 3,
+                    "imgUrl": "1-3",
+                    "title": "中国成人体重指数和腰围界限值与相关疾病危险的关系",
+                    "olink": null
+                },
+                {
+                    "id": 4,
+                    "imgUrl": "1-4",
+                    "title": "EuroSCORE心血管手术风险评分",
+                    "olink": null
+                },
+                {
+                    "id": 5,
+                    "imgUrl": "1-5",
+                    "title": "EuroSCORE II 心血管手术风险评分",
+                    "olink": null
+                },
+                {
+                    "id": 6,
+                    "imgUrl": "1-6",
+                    "title": "Framingham10年心血管疾病风险评分量表",
+                    "olink": null
+                },
+                {
+                    "id": 7,
+                    "imgUrl": "1-7",
+                    "title": "ASCVD风险评估、治疗目标、考虑药物治疗的胆固醇水平",
+                    "olink": null
+                },
+                {
+                    "id": 8,
+                    "imgUrl": "1-8",
+                    "title": "GRACE死亡风险评分",
+                    "olink": null
+                },
+                {
+                    "id": 9,
+                    "imgUrl": "1-9",
+                    "title": "CRUSADE院内出血风险评分",
+                    "olink": null
+                },
+                {
+                    "id": 10,
+                    "imgUrl": "1-10",
+                    "title": "急性心肌梗死Killip心功能分级",
+                    "olink": null
+                },
+                {
+                    "id": 11,
+                    "imgUrl": "1-11",
+                    "title": "慢性心力衰竭NYHA心功能分级",
+                    "olink": null
+                },
+                {
+                    "id": 12,
+                    "imgUrl": "1-12",
+                    "title": "房颤患者1年严重出血风险HAS-BLED评分表",
+                    "olink": null
+                },
+                {
+                    "id": 13,
+                    "imgUrl": "1-13",
+                    "title": "UA/NSTEMI患者死亡和心脏缺血事件TIMI评分表",
+                    "olink": null
+                },
+                {
+                    "id": 14,
+                    "imgUrl": "1-14",
+                    "title": "STEMI患者30天死亡风险TIMI评分表",
+                    "olink": null
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "title": "神经内科",
+            "subTitle": [
+                {
+                    "id": 1,
+                    "imgUrl": "2-1",
+                    "title": "急性脑卒中神经功能Mathew量表（MS）",
+                    "olink": null
+                },
+                {
+                    "id": 2,
+                    "imgUrl": "2-2",
+                    "title": "卒中患者运动评分（MAS）",
+                    "olink": null
+                },
+                {
+                    "id": 3,
+                    "imgUrl": "2-3",
+                    "title": "美国国立卫生研究院卒中量表（NIHSS）",
+                    "olink": null
+                },
+                {
+                    "id": 4,
+                    "imgUrl": "2-4",
+                    "title": "日常生活能力评定量表（Barthel 指数）",
+                    "olink": null
+                },
+                {
+                    "id": 5,
+                    "imgUrl": "2-5",
+                    "title": "Glasgow昏迷评分（GCS）",
+                    "olink": null
+                },
+                {
+                    "id": 6,
+                    "imgUrl": "2-6",
+                    "title": "Essen卒中风险评估量表",
+                    "olink": null
+                },
+                {
+                    "id": 7,
+                    "imgUrl": "2-7",
+                    "title": "TIA卒中风险评估（ABCD2）",
+                    "olink": null
+                },
+                {
+                    "id": 8,
+                    "imgUrl": "2-8",
+                    "title": "房颤患者卒中风险评分（CHADS2）",
+                    "olink": null
+                },
+                {
+                    "id": 9,
+                    "imgUrl": "2-9",
+                    "title": "房颤患者卒中风险评分（CHA2DS2VASc）",
+                    "olink": null
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "title": "内分泌科",
+            "subTitle": [
+                {
+                    "id": 1,
+                    "imgUrl": "3-1",
+                    "title": "糖代谢状态分类（WHO 1999）",
+                    "olink": null
+                },
+                {
+                    "id": 2,
+                    "imgUrl": "3-2",
+                    "title": "糖化血红蛋白（HbA1c）与平均血糖关系对照表",
+                    "olink": null
+                },
+                {
+                    "id": 3,
+                    "imgUrl": "3-3",
+                    "title": "糖尿病临床监测方案表",
+                    "olink": null
+                },
+                {
+                    "id": 4,
+                    "imgUrl": "3-4",
+                    "title": "糖尿病饮食管理--等热量食物交换表",
+                    "olink": null
+                }
+            ]
+        }
+    ]
+}
